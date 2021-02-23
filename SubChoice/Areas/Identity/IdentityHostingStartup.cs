ee4165c20ca84bb1552b8f370a9b9cb9bd0f0e98
Sviatoslav Hamar <sviatoslavgamar2001@gmail.com>
2021-02-23 23:24:32 +0200

    add project

---
+++ SubChoice/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SubChoice.Data;
+
+[assembly: HostingStartup(typeof(SubChoice.Areas.Identity.IdentityHostingStartup))]
+namespace SubChoice.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
