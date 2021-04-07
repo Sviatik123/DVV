706d5e34c80c1ec9a75fba65cda24dbeb20bd547
Daria Penkova <47561214+dashapenkova@users.noreply.github.com>
2021-04-07 13:20:07 +0300

    Logger added

---
+++ SubChoice.Services/LoggerService.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using SubChoice.Core.Interfaces.Services;
+using SubChoice.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubChoice.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        public LoggerService()
+        {
+            //Log.Logger = new LoggerConfiguration()
+            //.WriteTo.File("Logs/log.txt")
+            //.CreateLogger();
+        }
+
+        public void LogInfo(string msg)
+        {
+            Log.Information(msg);
+        }
+
+        public void LogError(string msg)
+        {
+            Log.Error(msg);
+        }
+
+        public void LogFatal(string msg)
+        {
+            Log.Fatal(msg);
+        }
+    }
+}
