87d070d863c55b4fe0248fefc235a8150979f352
Daria Penkova <47561214+dashapenkova@users.noreply.github.com>
2021-05-11 02:58:54 +0300

    Update BaseEntityExtension.cs

--- SubChoice.Core/Extensions/BaseEntityExtension.cs
+++ SubChoice.Core/Extensions/BaseEntityExtension.cs
@@ -24,7 +24,7 @@ namespace SubChoice.Core.Extensions
                     }
                     catch (Exception)
                     {
-                        // ignored,
+                        // ignored
                     }
                 }
             }
